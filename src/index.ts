b9e63c52cfb70f1186e87e0b78cd97a1e28c7501
smurphyk <sean.murphy131@gmail.com>
2020-11-19 14:45:56 -0500

    Set up initial configuration for mikroORM

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world')
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants'
+import { Post } from './entities/post'
+import microConfig from './mikro-orm.config'
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig)
+
+  // Create post
+  const post = orm.em.create(Post, { title: 'my first post' })
+  // Send post to DB
+  await orm.em.persistAndFlush(post)
+}
+
+main().catch((err) => {
+  console.error(err)
+})
